bf208cc7f8088894dd41d08fdd63999c7a721e3c
softlolx <softlolx@gmail.com>
2022-09-16 12:26:30 +0300

    initial layout

---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <section className={styles.content}></section>
+    </div>
+  );
+}
+
+export default App;
