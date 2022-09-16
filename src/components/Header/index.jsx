d1a6726d40e7368025bcb0846b6487f3fb0eac2f
softlolx <softlolx@gmail.com>
2022-09-16 17:30:35 +0300

    header final

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,20 @@
 import styles from "./Header.module.scss";
+import logo from "../../images/logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.header__logoContainer}>
-        <img src="#" alt="#" className={styles.header__logo} />
+        <img src={logo} alt="#" className={styles.header__logo} />
         <div className={styles.header__titleContainer}>
           <h1 className={styles.header__title}>react dranik</h1>
-          <p className={styles.header__subtitle}>Самые крутые драники в мире</p>
+          <p className={styles.header__subtitle}>Самые крутые драники</p>
         </div>
       </div>
 
       <button className={styles.cartButton}>
-        <div className={styles.cartButton__sumContainer}>
-          <span className={styles.cartButton__sum}>&#8381; 520</span>
-        </div>
+        <span className={styles.cartButton__sum}>&#8381;520</span>
+        <div className={styles.cartButton__line}></div>
         <div className={styles.cartButton__qtyContainer}>
           <div className={styles.cartButton__cartIcon}></div>
           <span className={styles.cartButton__totalQty}>3</span>
