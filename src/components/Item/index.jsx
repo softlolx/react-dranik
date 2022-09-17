473e49e45d76c11af9c4c6b42b21158fdc02aa3d
softlolx <softlolx@gmail.com>
2022-09-17 15:10:14 +0300

    feat: initial rendering from fake api

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import styles from "./Item.module.scss";
 
-export function Item({ imgUrl, name, price }) {
+export function Item({ imgUrl, name, price, types, sizes, category, rating }) {
   const [addCount, setAddCount] = useState(0);
+
+  const specs = ["диетические", "со\u00A0сметаной"];
 
   function handleAddButtonCLick() {
     setAddCount((prev) => prev + 1);
@@ -13,25 +15,20 @@ export function Item({ imgUrl, name, price }) {
       <img src={imgUrl} alt="#" className={styles.item__image} />
       <h3 className={styles.item__name}>{name}</h3>
       <div className={styles.item__configContainer}>
-        <button
-          className={`${styles.item__configButton} ${styles.item__configButton_wide} ${styles.item__configButton_active}`}
-        >
-          диетические
-        </button>
-        <button className={`${styles.item__configButton} ${styles.item__configButton_wide} `}>
-          со сметаной
-        </button>
-        <button
-          className={`${styles.item__configButton} ${styles.item__configButton_short} ${styles.item__configButton_active}`}
-        >
-          M
-        </button>
-        <button className={`${styles.item__configButton} ${styles.item__configButton_short}`}>
-          L
-        </button>
-        <button className={`${styles.item__configButton} ${styles.item__configButton_short}`}>
-          XL
-        </button>
+        <div className={styles.item__specificButtons}>
+          {types.map((item) => {
+            return <button className={styles.item__specificButton}>{specs[item]}</button>;
+          })}
+        </div>
+        <div className={styles.item__sizes}>
+          {sizes.map((item, index) => {
+            return (
+              <button className={styles.item__sizeButton} key={index}>
+                {item}
+              </button>
+            );
+          })}
+        </div>
       </div>
       <div className={styles.item__bottomContainer}>
         <span className={styles.item__price}>от&#160;{price}&#8381;</span>
