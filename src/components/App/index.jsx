97079a9e68801f2829337d24763739d16236c282
softlolx <softlolx@gmail.com>
2022-09-20 16:07:42 +0300

    feat: sceleton on loading

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./App.module.scss";
-import { data } from "../../utils/fakeApi";
 
 import { Header } from "../Header";
 import { Categories } from "../Categories";
 import { Main } from "../Main";
 import { Item } from "../Item";
+import ItemSceleton from "../Item/ItemSceleton";
 import { Cart } from "../Cart";
 import { CartItem } from "../CartItem";
 import { SortPopup } from "../SortPopup/SortPopup";
 
+const BASE_URL = "https://6323b8a1bb2321cba91e1779.mockapi.io";
+
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (function getItems() {
+      fetch(`${BASE_URL}/items`)
+        .then((res) => {
+          if (!res.ok) {
+            console.log(res.status);
+            return;
+          } else {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          setItems(data);
+          setIsLoading(false);
+        })
+        .catch((error) => console.log(error));
+    })();
+  }, []);
+
   const [isSortPopupOpened, setIsSortPopupOpened] = useState(false);
   const [selectedSortOption, setSelectedSortOption] = useState("популярности");
 
@@ -36,21 +60,23 @@ function App() {
           <CartItem />
         </Cart> */}
         <Main>
-          {data.map((item) => {
-            return (
-              <Item
-                key={item.id}
-                id={item.id}
-                imgUrl={item.imageUrl}
-                name={item.title}
-                price={item.price}
-                types={item.types}
-                sizes={item.sizes}
-                category={item.category}
-                rating={item.rating}
-              />
-            );
-          })}
+          {isLoading
+            ? [...new Array(8)].map((_, i) => <ItemSceleton key={i} />)
+            : items?.map((item) => {
+                return (
+                  <Item
+                    key={item.id}
+                    id={item.id}
+                    imgUrl={item.imageUrl}
+                    name={item.title}
+                    price={item.price}
+                    types={item.types}
+                    sizes={item.sizes}
+                    category={item.category}
+                    rating={item.rating}
+                  />
+                );
+              })}
         </Main>
       </section>
     </div>
---
+++ src/components/Item/ItemSceleton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+import styles from "./Item.module.scss";
+
+const ItemSceleton = (props) => (
+  <ContentLoader
+    className={styles.item}
+    speed={2}
+    width={400}
+    height={400}
+    viewBox="0 0 148 300"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <rect x="5" y="6" rx="20" ry="20" width="140" height="120" />
+    <rect x="5" y="140" rx="10" ry="10" width="140" height="40" />
+    <rect x="6" y="195" rx="10" ry="10" width="140" height="62" />
+    <rect x="83" y="271" rx="7" ry="7" width="65" height="28" />
+    <rect x="8" y="271" rx="7" ry="7" width="50" height="28" />
+  </ContentLoader>
+);
+
+export default ItemSceleton;
