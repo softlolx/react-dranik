72cd328c7fdd1b28320bed6fbc2bd46f25e31cea
softlolx <softlolx@gmail.com>
2022-09-23 00:43:40 +0300

    quckfix link size

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -2,12 +2,6 @@ import ReactPaginate from "react-paginate";
 import styles from "./Pagination.module.scss";
 
 export function Pagination({ onPageChange }) {
-  // const handlePageClick = (event) => {
-  //   const newOffset = (event.selected * 4) % items.length;
-  //   console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
-  //   setItemOffset(newOffset);
-  // };
-
   return (
     <>
       <ReactPaginate
