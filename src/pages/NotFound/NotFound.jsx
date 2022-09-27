ff2635629708f180418b108b12f189eba56ec3e4
softlolx <softlolx@gmail.com>
2022-09-27 21:05:35 +0300

    fix: 404 page styling

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export function NotFound() {
   return (
     <div className={styles.notFound}>
+      <span className={styles.notFound__404}>404</span>
       <h2 className={styles.notFound__title}>Вы попали в бездну отчаяния</h2>
       <Link to={"/"}>
         <button className={styles.notFound__button}>Назад в светлое будущее</button>
