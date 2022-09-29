491de78ad7545b751e0cb6275721b90109a2e6c0
softlolx <softlolx@gmail.com>
2022-09-29 11:43:17 +0300

    feat: searchbar focus inload on desktop

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import { useRef } from "react";
 import styles from "./SearchBar.module.scss";
 
 export function SearchBar({ searchBarValue, onSearchBarChange }) {
+  const searchBarRef = useRef();
+
+  useEffect(() => {
+    if (window.innerWidth > 1024) {
+      searchBarRef.current.focus();
+    }
+  }, []);
+
   return (
     <input
+      ref={searchBarRef}
       className={styles.search__input}
       placeholder="Поиск..."
       onChange={(e) => onSearchBarChange(e)}
