095d1ab24db27a56c192fe87709a545b3f376510
softlolx <softlolx@gmail.com>
2022-09-30 15:29:08 +0300

    basic cart items rendering

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -1,25 +1,23 @@
 import styles from "./CartItem.module.scss";
 
-export function CartItem() {
+export function CartItem({ id, title, price, imgUrl, type, size }) {
   return (
     <div className={styles.cartItem}>
       <div className={styles.product}>
-        <img
-          src="https://cdn.vkuso.ru/uploads/draniki-s-syrom-vetchinoj-i-ukropom-820.jpg"
-          alt="#"
-          className={styles.product__image}
-        />
+        <img src={imgUrl} alt="#" className={styles.product__image} />
         <div className={styles.product__descriptionContainer}>
-          <p className={styles.product__name}>Классика</p>
-          <span className={styles.product__specs}>XL, со сметаной</span>
+          <p className={styles.product__name}>{title}</p>
+          <span className={styles.product__specs}>
+            {size}, {type}
+          </span>
         </div>
       </div>
       <div className={styles.cartItem__qtyContainer}>
         <button className={styles.cartItem__removeQtyButton}></button>
-        <span className={styles.cartItem__qty}>2</span>
+        <span className={styles.cartItem__qty}>1</span>
         <button className={styles.cartItem__addQtyButton}></button>
       </div>
-      <span className={styles.cartItem__sum}>550</span>
+      <span className={styles.cartItem__sum}>{price}</span>
       <button className={styles.cartItem__removeButton}></button>
     </div>
   );
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,7 +1,11 @@
 import styles from "./Cart.module.scss";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { CartItem } from "../../components/CartItem";
 
 export function Cart({ children }) {
+  const { cartItems, totalQty, totalPrice } = useSelector((state) => state.cart);
+
   return (
     <section className={styles.cart}>
       <div className={styles.cart__header}>
@@ -11,11 +15,25 @@ export function Cart({ children }) {
         </div>
         <button className={styles.cart__clearButton}>Очистить корзину</button>
       </div>
-      <div className={styles.cartItemsContainer}>{children}</div>
+      <div className={styles.cartItemsContainer}>
+        {cartItems.map((item) => {
+          return (
+            <CartItem
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              imgUrl={item.imgUrl}
+              type={item.type}
+              size={item.size}
+            />
+          );
+        })}
+      </div>
       <div className={styles.cart__footer}>
         <div className={styles.cart__totalContainer}>
-          <span className={styles.cart__totalQty}>Всего порций: 3 шт</span>
-          <span className={styles.cart__totalSum}>Сумма заказа: 900 р</span>
+          <span className={styles.cart__totalQty}>Всего порций: {totalQty} шт</span>
+          <span className={styles.cart__totalSum}>Сумма заказа: {totalPrice} р</span>
         </div>
         <div className={styles.cart__buttonContainer}>
           <Link to={"/"}>
